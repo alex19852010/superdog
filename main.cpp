92d49d0dca0dddcd28b8be28083c2e2e92411b5c
Alex Bogushenko <bogushenkoas@ukr.net>
2023-10-24 11:11:56 +0300

    inner bramch

--- main.cpp
+++ main.cpp
@@ -3,63 +3,58 @@
 
 using namespace std;
 
-
-class Talent {
-public:
-    virtual void show_talent() = 0;
+class Weapon
+{
+  public:
+  virtual void shoot() = 0;
+  
 };
 
-class Swimming : public Talent {
+class Gun : public Weapon
+{  
 public:
-    void show_talent() override {
-        cout << "It can swim" << endl;
-    }
+ void shoot() override
+{
+  cout << "bang" << endl;
+}
 };
 
-class Dancing : public Talent {
+class SubmashineGan : public Gun
+{ 
 public:
-    void show_talent() override {
-        cout << "It can dance" << endl;
-    }
+ void shoot() override
+{
+  cout << "bang bang" << endl;
+}
 };
 
-class Counting : public Talent {
+class Bazooka : public Weapon
+{ 
 public:
-    void show_talent() override {
-        cout << "It can count" << endl;
-    }
+ void shoot() override
+{
+  cout << "badaboom" << endl;
+}
 };
 
-class Dog {
-private:
-    string name;
-    vector<Talent*> talents;
-
+class Pleer
+{
 public:
-    Dog(const string& name) : name(name) {}
-
-    void add_talent(Talent* talent) {
-        talents.push_back(talent);
-    }
-
-    void show_talents() {
-        cout << "This is " << name << " and it has some talents:" << endl;
-        for (const auto& talent : talents) {
-            talent->show_talent();
-        }
-    }
+ void shoot(Weapon * weapon) 
+{
+  weapon->shoot();
+}
 };
 
 int main()
 {
  
-    Dog dog1("Steve");
+  Pleer pleer;
+  Gun gun;
+  SubmashineGan submashineGan;
+  Bazooka bazooka;
+  pleer.shoot(& bazooka);
 
-    dog1.add_talent(new Dancing());
-    dog1.add_talent(new Swimming());
-    dog1.add_talent(new Counting());
-    dog1.show_talents();
-    
  
    return 0; 
 
